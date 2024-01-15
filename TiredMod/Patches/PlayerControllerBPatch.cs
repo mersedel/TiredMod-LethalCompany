8fde1a24fcae70e4f6f317da70dbe57472da599f
mersedel <xr6033851@gmail.com>
2024-01-15 23:36:14 +0200

    Created

---
+++ TiredMod/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,18 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace TiredMod.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+
+        static void NoSprintAndJumpPatch(ref float ___jumpForce, ref float ___sprintTime)
+        {
+            ___jumpForce = 1f;
+            ___sprintTime = 0f;
+        }
+    }
+}
---
+++ TiredMod/Plugin.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using TiredMod.Patches;
+
+namespace TiredMod
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class TiredModBase : BaseUnityPlugin
+    {
+        private const string modGUID = "mersedel.TiredMod";
+        private const string modName = "Tired mod";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static TiredModBase Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("The Tired Mod has awaken!");
+
+            harmony.PatchAll(typeof(TiredModBase));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+        }
+    }
+}
